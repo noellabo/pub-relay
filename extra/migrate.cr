a72e66e01df40aed2ee9d7913ebb218d20f1587c
RX14 <chris@rx14.co.uk>
2018-09-06 18:23:28 +0100

    Add logging to migration script

--- extra/migrate.cr
+++ extra/migrate.cr
@@ -7,10 +7,12 @@ old_redis.keys("subscription:*").each do |old_key|
   inbox_url = old_redis.hget(old_key, "inbox_url").not_nil!
 
   new_key = "relay:#{old_key}"
-  new_redis.hmset(new_key, {
+  new_state = {
     inbox_url:       inbox_url,
     follow_id:       "",
     follow_actor_id: "",
     state:           "Subscribed",
-  })
+  }
+  puts "HMSET #{new_key} #{new_state}"
+  new_redis.hmset(new_key, new_state)
 end
