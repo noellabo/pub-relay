8babcaaef01d9a9f766df4ef53b03577470a4404
Takeshi Umeda <noel.yoshiba@gmail.com>
2020-02-01 16:46:59 +0900

    fix: Synchronize log output without buffering if RELAY_DEBUG is enabled (#3)

--- src/entrypoint.cr
+++ src/entrypoint.cr
@@ -13,6 +13,7 @@ port = (ENV["RELAY_PORT"]? || 8085).to_i
 private_key_path = ENV["RELAY_PKEY_PATH"]
 private_key = OpenSSL::PKey::RSA.new(File.read(private_key_path))
 
+STDOUT.sync = true if ENV["RELAY_DEBUG"]?
 Earl::Logger.level = Earl::Logger::Severity::DEBUG if ENV["RELAY_DEBUG"]?
 
 relay = PubRelay.new(domain, private_key, redis, bindhost, port)
