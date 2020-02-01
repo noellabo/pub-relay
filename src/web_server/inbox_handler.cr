7d35711a27ad1abeb06a746ca19e2e37cbbc44aa
Takeshi Umeda <noel.yoshiba@gmail.com>
2020-02-01 16:39:32 +0900

    feat: Add Pleroma support (#2)

--- src/web_server/inbox_handler.cr
+++ src/web_server/inbox_handler.cr
@@ -1,5 +1,7 @@
 require "../activity"
 require "./http_signature"
+require "uuid"
+require "uuid/json"
 
 class PubRelay::WebServer::InboxHandler
   def initialize(
@@ -24,10 +26,14 @@ class PubRelay::WebServer::InboxHandler
     case activity
     when .follow?
       handle_follow(actor_from_signature, activity)
-    when .unfollow?
+    when .unfollow?, .reject?
       handle_unfollow(actor_from_signature, activity)
+    when .accept?
+      handle_accept(actor_from_signature, activity)
     when .valid_for_rebroadcast?
       handle_forward(actor_from_signature, request_body)
+    when .valid_for_relay?
+      handle_relay(actor_from_signature, activity)
     end
 
     response.status_code = 202
@@ -35,12 +41,21 @@ class PubRelay::WebServer::InboxHandler
   end
 
   def handle_follow(actor, activity)
-    unless activity.object_id == Activity::PUBLIC_COLLECTION
-      error(400, "Follow only allowed for #{Activity::PUBLIC_COLLECTION}")
-    end
-
     inbox_url = URI.parse(actor.inbox_url) rescue nil
     error(400, "Inbox URL was not a valid URL") unless inbox_url
+
+    if actor.pleroma_relay?
+      @subscription_manager.send(
+        SubscriptionManager::FollowSent.new(
+          domain: actor.domain,
+          inbox_url: inbox_url,
+          following_id: route_url("/#{UUID.random}"),
+          following_actor_id: actor.id
+        )
+      )
+    elsif activity.object_id != Activity::PUBLIC_COLLECTION
+      error(400, "Follow only allowed for #{Activity::PUBLIC_COLLECTION}")
+    end
 
     @subscription_manager.send(
       SubscriptionManager::Subscription.new(
@@ -49,6 +64,12 @@ class PubRelay::WebServer::InboxHandler
         follow_id: activity.id,
         follow_actor_id: actor.id
       )
+    )
+  end
+
+  def handle_accept(actor, activity)
+    @subscription_manager.send(
+      SubscriptionManager::AcceptReceive.new(actor.domain)
     )
   end
 
@@ -61,6 +82,15 @@ class PubRelay::WebServer::InboxHandler
   def handle_forward(actor, request_body)
     @subscription_manager.send(
       SubscriptionManager::Deliver.new(request_body, source_domain: actor.domain)
+    )
+  end
+
+  def handle_relay(actor, activity)
+    @subscription_manager.send(
+      SubscriptionManager::Announce.new(
+        object: activity.object_id.not_nil!.to_s,
+        source_domain: actor.domain
+      )
     )
   end
 
